e8b06da1edd998420191188b6c6aef39681629f2
Enrico Wiegmann <enrico.wiegmann@ssi-schaefer.com>
2024-12-13 10:15:57 +0100

    added login for existing user

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path
+from django.contrib.auth.decorators import login_required
+from django.urls import path, include
 from . import views
 
 urlpatterns= [
     path('list/', views.BlogListView.as_view(), name='blog_list'),
     path('<int:pk>/', views.BlogDetailView.as_view(), name='blog_detail'),
-    path('create/', views.BlogCreateView.as_view(), name='blog_create'),
+    path('create/', login_required(views.BlogCreateView.as_view()), name='blog_create'),
     path('<int:pk>/delete/', views.BlogDeleteView.as_view(), name='blog_delete'),
+    path('accounts/', include('django.contrib.auth.urls')),
 
     path('dashboard/', views.BlogDashboardView.as_view(), name='blog_dashboard'),
 
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, DeleteView
