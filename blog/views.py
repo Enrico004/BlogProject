ab43d56ed39e92063e5f49b66451769eeb55267a
Enrico Wiegmann <enrico.wiegmann@ssi-schaefer.com>
2025-01-10 12:47:47 +0100

    added further comments

--- blog/views.py
+++ blog/views.py
@@ -12,23 +12,37 @@ from django.views.generic import ListView, DetailView, CreateView, DeleteView
 from blog.forms import BlogForm, UpdateUserForm, UpdateProfileForm
 from blog.models import Blog
 
-
-# Create your views here.
 class BlogListView(ListView):
+    """
+    View-Klasse für die Liste aller Beiträge
+    """
     model = Blog
     template_name = 'blog/blog_list.html'
     context_object_name = 'blogs'
 
     def get_queryset(self):
+        """
+        Überschreibt die Standard-Methode.
+        Sammelt alle Beiträge des Nutzers, fügt die Anzahl der Likes hinzu und ordnet sie nach dem Erstellungsdatum
+        :return: Liste von Blog-Objekten
+        """
         return Blog.objects.annotate(like_count=Count('likes')).order_by('created_date')
 
 class BlogDetailView(DetailView):
+    """
+    View-Klasse zur Darstellung einer Detail-View
+    """
     model = Blog
     context_object_name = 'blog'
     template_name = 'blog/blog_detail.html'
 
 
     def get_object(self, queryset=None):
+        """
+        Überschreibt Standard-Methode. Inkrementiert die Anzahl an Aufrufen, sobald Detail-Seite aufgerufen wird
+        :param queryset:
+        :return: Beitrag mit inkrementiert Anzahl an aufrufen
+        """
         # Hole das Blog-Objekt
         blog = super().get_object(queryset)
         # Erhöhe die Klickanzahl
@@ -37,32 +51,42 @@ class BlogDetailView(DetailView):
         return blog
 
     def get_context_data(self, **kwargs):
+        """
+        Überschreibt Standard-Methode. Fügt die Anzahl der Likes zum Kontext hinzu
+        :param kwargs:
+        :return: Überarbeiteter Kontext
+        """
         data = super().get_context_data(**kwargs)
         data = add_likes_to_data_context(self.kwargs['pk'],self.request.user.id,data)
         return data
 
-def blog_post_like(request, pk):
-    post = get_object_or_404(Blog, id=request.POST.get('blogpost_id'))
-    if post.likes.filter(id=request.user.id).exists():
-        post.likes.remove(request.user)
-    else:
-        post.likes.add(request.user)
-
-    return JsonResponse({'like-count':post.likes.count()})
 
 class BlogCreateView(CreateView):
+    """
+    View-Klasse zum Erstellen eines Beitrags
+    """
     model = Blog
     form_class = BlogForm
     template_name = 'blog/blog_create.html'
     success_url = reverse_lazy('blogs:blog_list')
 
     def form_valid(self, form):
+        """
+        Überschreiben der Standard-Methode. Fügt aktuellen Nutzer als Autor hinzu
+        :param form: Aktuelle bearbeitete From
+        :return: HttpResponseRedirect abhängig von der Validität
+        """
         # Get authenticated user and set it as the blog author
         # authenticated user is guaranteed because of login_required in urls.py
         form.instance.author = self.request.user
         return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
+        """
+        Bearbeitet den bereits erstellten Kontext. Setzt die Anzahl an Aufrufen auf 0
+        :param kwargs:
+        :return: Überarbeiter Kontext
+        """
         context = super().get_context_data(**kwargs)
         # Übergibt ein leeres Blog-Objekt für den Zugriff auf Klicks/Likes
         context['blog'] = self.object or Blog(clicks=0)
@@ -71,6 +95,12 @@ class BlogCreateView(CreateView):
 
 @login_required
 def blog_post_like(request, pk):
+    """
+    Funktion zum Liken/Unliken eines Beitrags. Wenn der User den Beitrag bereits geliked hat, wird dieser entfernt. Ansonsten wird ein Like hinzugefügt
+    :param request: Anfrage vom Frontend
+    :param pk: Primärschlüssel des Beitrags
+    :return: JSON mit der Anzahl an Likes(like_count) und ob der User den Beitrag geliked hat(liked)
+    """
     blog = get_object_or_404(Blog, pk=pk)
 
     if blog.likes.filter(id=request.user.id).exists():
@@ -86,25 +116,36 @@ def blog_post_like(request, pk):
 
 
 class BlogDeleteView(DeleteView):
+    """
+    Klasse zum Löschen von Beiträgen
+    """
     model = Blog
     success_url = reverse_lazy('blogs:blog_list')
     template_name = 'blog/blog_delete.html'
 
-    def get_object(self, queryset=None):
-        # Increment clicks only if the user is visiting (not liking)
-        blog = super().get_object(queryset)
-        if not self.request.user.is_authenticated or self.request.method == "GET":
-            blog.clicks += 1
-            blog.save()
-        return blog
 
 class BlogDashboardView(ListView):
+    """
+    View-Klasse für die Dashboard-View
+    """
     model = Blog
     template_name = 'blog/blog_dashboard.html'  # Verwendet das neue Template
     context_object_name = 'latest_blogs'  # Kontextname für das QuerySet im Template
+
     def get_queryset(self):
+        """
+        Überschreibt die Standard-Methode.
+        Sammelt alle Beiträge, fügt die Anzahl der Likes hinzu und ordnet sie nach dem Erstellungsdatum
+        :return: Liste von Blog-Objekten
+        """
         return Blog.objects.annotate(like_count=Count('likes')).order_by('created_date')
+
     def get_context_data(self, **kwargs):
+        """
+        Überschreibt standard-Methode. Fügt dem Kontext die 5 letzten und 5 meistgeklickten Beiträge hinzu
+        :param kwargs:
+        :return: Kontext zur Verarbeitung im Frontend
+        """
         context = super().get_context_data(**kwargs)
         # Neueste 5 Blogs
         context['latest_blogs'] = Blog.objects.all().annotate(like_count=Count('likes')).order_by('-created_date')[:5]
